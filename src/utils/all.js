c657e04cfd9508fd1839503ea9ac410cda0c966f
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2024-04-07 16:38:21 +0200

    Fix weird Date Bug

--- src/utils/all.js
+++ src/utils/all.js
@@ -1,9 +1,17 @@
 /** */
-export const getFormattedDate = (date) =>
-  date
-    ? new Date(date).toLocaleDateString("de-de", {
+export const getFormattedDate = (dateString) => {
+  const parts = dateString.split(".");
+  if (parts.length === 3) {
+    const [day, month, year] = parts.map(part => parseInt(part, 10));
+    const date = new Date(year, month - 1, day); // Monate sind 0-basiert in JavaScript
+    if (!isNaN(date)) {
+      return date.toLocaleDateString("de-de", {
         year: "numeric",
         month: "short",
         day: "numeric",
-      })
-    : "";
+      });
+    }
+  }
+  console.error("Ungültiges Datum: ", dateString);
+  return ""; // Oder eine passende Fehlerbehandlung / Standardwert
+};
