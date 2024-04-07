b80f7f0b7d54a20d75f7ff9b5ffc75590961634b
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2024-04-07 17:08:15 +0200

    Date fix

--- src_template/utils/all.js
+++ src_template/utils/all.js
@@ -1,9 +1,9 @@
 /** */
 export const getFormattedDate = (date) =>
-date
-  ? new Date(date).toLocaleDateString("de-DE", {
-      year: "numeric",
-      month: "long", // Changed to "long" for the full month name.
-      day: "2-digit", // Changed to "2-digit" for the day to include leading zeros.
-    })
-  : "";
+  date
+    ? new Date(date).toLocaleDateString("en-us", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })
+    : "";
