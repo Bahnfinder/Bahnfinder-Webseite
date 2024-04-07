90923130db9458be048370fdfbe9531f0ca2ab69
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2024-04-07 16:51:53 +0200

    Another Test

--- src/utils/all.js
+++ src/utils/all.js
@@ -1,18 +1,9 @@
-export const getFormattedDate = (dateString) => {
-  if (!dateString) return "";
-
-  // Split the date string to access day, month, and year individually
-  const parts = dateString.split(".");
-  // Adjust the format to "mm/dd/yyyy" which is reliably parsed by the Date constructor across environments
-  const formattedDateString = `${parts[1]}/${parts[0]}/${parts[2]}`;
-
-  // Now, create a Date object
-  const date = new Date(formattedDateString);
-
-  // Return the formatted date string
-  return date.toLocaleDateString("de-de", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
-};
+/** */
+export const getFormattedDate = (date) =>
+  date
+    ? new Date(date).toLocaleDateString("de-de", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })
+    : "";
